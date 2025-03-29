b57518336e60fccf427f098f1f6525946f0f3f22
amandashohdy <amandashohdy@gmail.com>
2025-03-29 13:14:50 -0700

    Output numbers increased slightly

---
+++ grading/CompliantNode.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CompliantNode implements Node {
+
+    public double p_graph ; 
+    public double p_malicious ; 
+    public double p_txDistribution ; 
+    public int numRounds ; 
+    public boolean[] followees ; 
+    public Set<Transaction> pendingTransactions ; 
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+        this.p_graph = p_graph ; 
+        this.p_malicious = p_malicious ; 
+        this.p_txDistribution = p_txDistribution ; 
+        
+        this.numRounds = numRounds;
+        this.pendingTransactions = new HashSet<Transaction>() ;
+    }
+
+    public void setFollowees(boolean[] followees) {
+        this.followees = followees ;
+    }
+
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+        //  this.pendingTransactions.addAll(pendingTransactions);
+        for (Transaction tx : pendingTransactions) { this.pendingTransactions.add(tx) ; }
+    }
+
+    public Set<Transaction> getProposals() {
+        return this.pendingTransactions ; 
+        // return new HashSet<>(pendingTransactions); // no filtering yet
+    }
+
+    public void receiveCandidates(ArrayList<Integer[]> candidates) {
+
+        /* for (Candidate c : candidates) { this.pendingTransaction.add(c.tx) ; } */
+
+        for (Integer[] pair : candidates) {
+            //int sender = pair[0];
+            Transaction tx = new Transaction(pair[1]) ;
+
+            //if (sender < 0 || sender >= followees.length) continue;
+            //if (!followees[sender]) continue;
+
+            this.pendingTransactions.add(tx);
+        }
+    }
+}
+
