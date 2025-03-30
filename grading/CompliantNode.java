27d5eb2579f8b8d516324cd1c2ce900a6fdce7b8
amandashohdy <amandashohdy@gmail.com>
2025-03-30 15:01:28 -0700

    All tests passed
    Updated README

--- grading/CompliantNode.java
+++ grading/CompliantNode.java
@@ -1,46 +1,43 @@
-import java.util.*;
+import java.util.Set ;
+import java.util.HashSet ;
+import java.util.ArrayList ;
 
-public class CompliantNode implements Node {
-    private Set<Integer> transactions; // Transactions this node believes are valid
-    private boolean[] followees; // Array to track which nodes are followed
-    private int numRounds; // Total simulation rounds
-    private int currentRound; // Current round count
+public class CompliantNode implements Node 
+{
+    private Set<Integer> transactions ;
+    private boolean[] followees ; 
 
-    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        this.numRounds = numRounds;
-        this.currentRound = 0;
-        this.transactions = new HashSet<>();
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) 
+    {
+        this.transactions = new HashSet<>() ;
     }
 
-    public void setFollowees(boolean[] followees) {
-        this.followees = followees;
+    public void setFollowees(boolean[] followees) 
+    {
+        this.followees = followees ;
     }
 
-    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        for (Transaction tx : pendingTransactions) {
-            transactions.add(tx.id);
-        }
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) 
+    {
+        for (Transaction tx : pendingTransactions) { transactions.add(tx.id) ; }
     }
 
-    public Set<Transaction> getProposals() {
-        // Convert the set of known transactions into a set of Transaction objects
-        Set<Transaction> proposals = new HashSet<>();
-        for (int txId : transactions) {
-            proposals.add(new Transaction(txId));
-        }
-        return proposals;
+    public Set<Transaction> getProposals() 
+    {
+        Set<Transaction> proposals = new HashSet<>() ;
+        for (int txId : transactions) { proposals.add(new Transaction(txId)) ; }
+        return proposals ;
     }
 
-    public void receiveCandidates(ArrayList<Integer[]> candidates) {
-        currentRound++; // Track rounds
-        for (Integer[] candidate : candidates) {
-            int txID = candidate[0]; // Transaction ID
-            int senderNode = candidate[1]; // Node that proposed this transaction
+    public void receiveCandidates(ArrayList<Integer[]> candidates) 
+    {
+        for (Integer[] candidate : candidates) 
+        {
+            int txID = candidate[0] ; 
+            int senderNode = candidate[1] ; 
 
             // Accept transaction if it came from a followee
-            if (followees[senderNode]) {
-                transactions.add(txID);
-            }
+            if (followees[senderNode]) { transactions.add(txID) ; }
         }
     }
 }
